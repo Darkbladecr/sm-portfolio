15ab5d0868b718545486503eaa22f01e3f7cebb0
Darkbladecr <smig88@gmail.com>
2020-04-15 14:52:57 +0100

    dockerize site

--- api/messages.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-const twilio = require('twilio');
-const client = new twilio(process.env.twilio_sid, process.env.twilio_auth);
-
-client.messages
-  .list({ limit: 20 })
-  .then(messages => messages.forEach(m => console.log(m)));
